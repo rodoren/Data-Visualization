e0d85739496aff01ffd2b526ed8c83ea5b060af6
rodoren <64158919+rodoren@users.noreply.github.com>
2021-04-23 20:27:09 -0500

    Update ejem_bayes.R

--- R/ejem_bayes.R
+++ R/ejem_bayes.R
@@ -14,7 +14,7 @@ posteriord<-dbeta(p,a+y,b+ny)
 #### Visualizacion de las distribuciones 
 # la seleccion de colores se pueden encontrar en el siguiente enlace:
 # http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
-plot(p,posteriord,type="l", lty=2,lwd=3,ylab="Densidad",col="darkcyan", main= "Ejemplo de Estadistica Bayesiana")
+plot(p,posteriord,type="l", lty=2,lwd=3,ylab="Densidad",col="darkcyan", main= "Ejemplo: Estadistica Bayesiana Distribución Beta")
 lines(p,like,lty=1,lwd=3,col="coral3")
 lines(p,priord,lty=3,lwd=3,col="chartreuse4")
 legend(0.7,4,c("Priori","verosimilitud","Posteriori"),lty=c(3,2,1),lwd=c(3,3,3),col=c("chartreuse4","coral3","darkcyan"),cex=0.7)
